0fa072257bcf9424ee31a7960aca7dc143bd03a4
Yakup Zengin <clafedersin@gmail.com>
2024-03-22 23:26:48 +0300

    Updated AccountService interface to include getAccountAll method for retrieving all accounts

--- src/main/java/com/example/banking/service/AccountService.java
+++ src/main/java/com/example/banking/service/AccountService.java
@@ -2,8 +2,11 @@ package com.example.banking.service;
 
 import com.example.banking.dto.AccountDto;
 
+import java.util.List;
+
 public interface AccountService {
 
     AccountDto createAccount(AccountDto account);
     AccountDto getAccountById(Long id);
+    List<AccountDto> getAccountAll();
 }
