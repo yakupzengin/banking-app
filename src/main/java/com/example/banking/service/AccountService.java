b5ab78c4f05ff3d7f9c1fd8ff5e19c85b33d2cdf
Yakup Zengin <clafedersin@gmail.com>
2024-03-23 00:02:30 +0300

    Updated AccountService interface to include deposit and withdraw methods for account transactions.

--- src/main/java/com/example/banking/service/AccountService.java
+++ src/main/java/com/example/banking/service/AccountService.java
@@ -9,4 +9,6 @@ public interface AccountService {
     AccountDto createAccount(AccountDto account);
     AccountDto getAccountById(Long id);
     List<AccountDto> getAccountAll();
+    AccountDto deposit(Long id, double amount);
+    AccountDto withdraw(Long id, double amount);
 }
