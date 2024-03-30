a93a55f864158d3ae63b55bb2770aad9857f7a91
Yakup Zengin <clafedersin@gmail.com>
2024-03-30 17:58:52 +0300

    Add transaction validation and account balance update functionality to TransactionServiceImpl

--- src/main/java/com/example/banking/service/iml/TransactionServiceImpl.java
+++ src/main/java/com/example/banking/service/iml/TransactionServiceImpl.java
@@ -4,10 +4,12 @@ import com.example.banking.dto.TransactionDto;
 import com.example.banking.entity.Account;
 import com.example.banking.entity.Transaction;
 import com.example.banking.enums.TransactionType;
+import com.example.banking.exception.BankingException;
 import com.example.banking.mapper.TransactionMapper;
 import com.example.banking.repository.AccountRepository;
 import com.example.banking.repository.TransactionRepository;
 import com.example.banking.service.TransactionService;
+import com.example.banking.validators.TransactionValidator;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -18,16 +20,52 @@ import java.util.List;
 public class TransactionServiceImpl implements TransactionService {
     private TransactionRepository transactionRepository;
     private AccountRepository accountRepository;
-    public TransactionServiceImpl(TransactionRepository transactionRepository, AccountRepository accountRepository) {
+    private TransactionValidator transactionValidator;
+    public TransactionServiceImpl(TransactionRepository transactionRepository, AccountRepository accountRepository, TransactionValidator transactionValidator) {
         this.transactionRepository=transactionRepository;
         this.accountRepository = accountRepository;
-    }
+        this.transactionValidator = transactionValidator;
 
+    }
+    @Override
+    public void createTransactionAndUpdateBalance(TransactionDto transactionDto) {
+        // Create transaction
+        createTransaction(transactionDto);
+
+        // Update account balance
+        updateAccountBalance(transactionDto);
+    }
     public TransactionDto createTransaction(TransactionDto transactionDto) {
+        // Perform validation before creating the transaction
+        if (!transactionValidator.validateTransaction(transactionDto)) {
+            throw new BankingException("Transaction validation failed during createTransaction operation");
+        }
+
         Transaction transaction = TransactionMapper.mapToTransaction(transactionDto);
         Transaction savedTransaction = transactionRepository.save(transaction);
         return TransactionMapper.mapToTransactionDto(savedTransaction);
+    }
 
+    public void updateAccountBalance(TransactionDto transactionDto) {
+        // Update account balance according to relevant transaction type
+        if (transactionDto.getTransactionType() == TransactionType.DEPOSIT) {
+            Account account = accountRepository.findById(transactionDto.getAccountId())
+                    .orElseThrow(() -> new BankingException("Account not found for deposit transaction"));
+
+            BigDecimal currentBalance = account.getBalance();
+            BigDecimal newBalance = currentBalance.add(transactionDto.getAmount());
+            account.setBalance(newBalance);
+            accountRepository.save(account);
+        } else if (transactionDto.getTransactionType() == TransactionType.WITHDRAWAL) {
+            Account account = accountRepository.findById(transactionDto.getAccountId())
+                    .orElseThrow(() -> new BankingException("Account not found for withdrawal transaction"));
+
+            BigDecimal currentBalance = account.getBalance();
+            BigDecimal newBalance = currentBalance.subtract(transactionDto.getAmount());
+
+            account.setBalance(newBalance);
+            accountRepository.save(account);
+        }
     }
     public TransactionDto getTransactionById(Long id){
         Transaction transaction = transactionRepository.findById(id)
@@ -43,28 +81,6 @@ public class TransactionServiceImpl implements TransactionService {
         return transactionDtos;
     }
 
-    public void updateAccountBalance(TransactionDto transactionDto){
-        // Update account balance according to relevant transaction type
-        if (transactionDto.getTransactionType() == TransactionType.DEPOSIT) {
-            Account account = accountRepository.findById(transactionDto.getAccountId()).orElse(null);
-            System.out.println("account = " + account);
-            if (account != null) {
-                BigDecimal currentBalance = account.getBalance();
-                BigDecimal newBalance = currentBalance.add(transactionDto.getAmount());
-                System.out.println("currentBalance : " + currentBalance);
-                System.out.println("newBalance : " + newBalance);
-                account.setBalance(newBalance);
-                accountRepository.save(account);
-            }
-        } else if (transactionDto.getTransactionType() == TransactionType.WITHDRAWAL) {
-            Account account = accountRepository.findById(transactionDto.getAccountId()).orElse(null);
-            if (account != null) {
-                BigDecimal currentBalance = account.getBalance();
-                BigDecimal newBalance = currentBalance.subtract(transactionDto.getAmount());
-                account.setBalance(newBalance);
-                accountRepository.save(account);
-            }
-        }
-    }
+
 
 }
