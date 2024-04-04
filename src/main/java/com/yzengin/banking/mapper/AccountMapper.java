7bfb2caf83f192249309426519a2de439a2789d2
Yakup Zengin <clafedersin@gmail.com>
2024-04-04 16:15:31 +0300

    Update AccountMapper to support conversion of additional fields in AccountDto and Account

--- src/main/java/com/yzengin/banking/mapper/AccountMapper.java
+++ src/main/java/com/yzengin/banking/mapper/AccountMapper.java
@@ -2,24 +2,24 @@ package com.yzengin.banking.mapper;
 
 import com.yzengin.banking.dto.AccountDto;
 import com.yzengin.banking.entity.Account;
+import com.yzengin.banking.entity.User;
 
 public class AccountMapper {
-    public static Account mapToAccount  (AccountDto accountDto){
-        Account account = new Account(
-                accountDto.getId(),
-                accountDto.getAccountHolderName(),
-                accountDto.getBalance()
-        );
-
+    public static Account mapToAccount(AccountDto accountDto) {
+        Account account = new Account();
+        account.setUser(new User(accountDto.getUserId()));
+        account.setBalance(accountDto.getBalance());
+        account.setCurrency(accountDto.getCurrency());
+        account.setOpenedAt(accountDto.getOpenedAt());
         return account;
     }
-    public static AccountDto mapToAccountDto(Account account){
-        AccountDto accountDto = new AccountDto(
-                account.getId(),
-                account.getAccountHolderName(),
-                account.getBalance()
-        );
+
+    public static AccountDto mapToAccountDto(Account account) {
+        AccountDto accountDto = new AccountDto();
+        accountDto.setUserId(account.getUser().getId());
+        accountDto.setBalance(account.getBalance());
+        accountDto.setCurrency(account.getCurrency());
+        accountDto.setOpenedAt(account.getOpenedAt());
         return accountDto;
     }
-
 }
