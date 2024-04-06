0a538bf921ce5729af99d417573ae1ab8bccb275
Yakup Zengin <clafedersin@gmail.com>
2024-04-07 02:58:15 +0300

    Added mapping method to convert a list of Account entities to a list of AccountDto objects, and a method to map AccountDto to Account entity.

--- src/main/java/com/yzengin/banking/mapper/AccountMapper.java
+++ src/main/java/com/yzengin/banking/mapper/AccountMapper.java
@@ -4,6 +4,9 @@ import com.yzengin.banking.dto.AccountDto;
 import com.yzengin.banking.entity.Account;
 import com.yzengin.banking.entity.User;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AccountMapper {
     public static Account mapToAccount(AccountDto accountDto) {
         Account account = new Account();
@@ -22,4 +25,18 @@ public class AccountMapper {
         accountDto.setOpenedAt(account.getOpenedAt());
         return accountDto;
     }
+    public static List<AccountDto> mapToAccountDtoList(List<Account> accounts) {
+        return accounts.stream()
+                .map(AccountMapper::mapToAccountDto) // Convert each Account object to AccountDto
+                .collect(Collectors.toList()); // Collect and return converted AccountDtos into a list
+    }
+    public static Account mapToAccountEntity(AccountDto accountDto) {
+        Account account = new Account();
+        account.setUser(new User(accountDto.getUserId()));
+        account.setBalance(accountDto.getBalance());
+        account.setCurrency(accountDto.getCurrency());
+        account.setOpenedAt(accountDto.getOpenedAt());
+        return account;
+    }
+
 }
