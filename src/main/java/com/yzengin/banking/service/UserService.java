9ec8b1c9954953dedc73ef54c56de87ebc025351
Yakup Zengin <clafedersin@gmail.com>
2024-04-06 17:09:16 +0300

    Add UserService interface with methods for user management operations such as creating, updating, and deleting users

--- src/main/java/com/yzengin/banking/service/UserService.java
+++ src/main/java/com/yzengin/banking/service/UserService.java
@@ -1,4 +1,26 @@
 package com.yzengin.banking.service;
 
+import com.yzengin.banking.dto.AccountDto;
+import com.yzengin.banking.dto.TransactionDto;
+import com.yzengin.banking.dto.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+    List<UserDto> getUserAll();
+
+    UserDto getUserById(Long id);
+
+    UserDto createAccount(UserDto userDto);
+    void deleteUserById(Long id);
+
+    UserDto updateUser(Long id, UserDto userDto);
+
+    List<AccountDto> getUserAccounts(Long id);
+
+    List<TransactionDto> getUserTransactions(Long id);
+
+    AccountDto addAccountToUser(Long id, AccountDto accountDto);
+
+    TransactionDto addTransactionToUser(Long id, TransactionDto transactionDto);
 }
