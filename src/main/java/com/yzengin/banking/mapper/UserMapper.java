293a812a583a9d9387699025331f1499279e2acf
Yakup Zengin <clafedersin@gmail.com>
2024-04-06 17:09:37 +0300

    Created UserMapper.java

--- src/main/java/com/yzengin/banking/mapper/UserMapper.java
+++ src/main/java/com/yzengin/banking/mapper/UserMapper.java
@@ -1,4 +1,9 @@
 package com.yzengin.banking.mapper;
 
+import com.yzengin.banking.dto.UserDto;
+import com.yzengin.banking.entity.User;
+
 public class UserMapper {
+    public static UserDto mapToAccountDto(User user) {
+    }
 }
