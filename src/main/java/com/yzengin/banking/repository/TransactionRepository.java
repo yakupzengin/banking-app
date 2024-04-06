8f8a5788f8cced40c7795202e58bb942b9fd9a5d
Yakup Zengin <clafedersin@gmail.com>
2024-04-07 02:59:03 +0300

    Added method findByAccountId() to TransactionRepository to retrieve transactions by account ID.

--- src/main/java/com/yzengin/banking/repository/TransactionRepository.java
+++ src/main/java/com/yzengin/banking/repository/TransactionRepository.java
@@ -3,6 +3,9 @@ package com.yzengin.banking.repository;
 import com.yzengin.banking.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
 
+    List<Transaction> findByAccountId(Long userId);
 }
