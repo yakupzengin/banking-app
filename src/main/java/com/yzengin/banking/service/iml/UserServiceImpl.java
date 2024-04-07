f4ca0b48a531b89276f8342b6129f74f34369368
Yakup Zengin <clafedersin@gmail.com>
2024-04-07 03:04:55 +0300

    Updated UserServiceImpl to include methods for CRUD operations on users, accounts, and transactions.

--- src/main/java/com/yzengin/banking/service/iml/UserServiceImpl.java
+++ src/main/java/com/yzengin/banking/service/iml/UserServiceImpl.java
@@ -3,25 +3,38 @@ package com.yzengin.banking.service.iml;
 import com.yzengin.banking.dto.AccountDto;
 import com.yzengin.banking.dto.TransactionDto;
 import com.yzengin.banking.dto.UserDto;
+import com.yzengin.banking.entity.Account;
+import com.yzengin.banking.entity.Transaction;
 import com.yzengin.banking.entity.User;
+import com.yzengin.banking.mapper.AccountMapper;
+import com.yzengin.banking.mapper.TransactionMapper;
 import com.yzengin.banking.mapper.UserMapper;
+import com.yzengin.banking.repository.AccountRepository;
+import com.yzengin.banking.repository.TransactionRepository;
 import com.yzengin.banking.repository.UserRepository;
 import com.yzengin.banking.service.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
-    public UserServiceImpl(UserRepository userRepository){
+    private TransactionRepository transactionRepository;
+    private AccountRepository accountRepository;
+    public UserServiceImpl(UserRepository userRepository,TransactionRepository transactionRepository,AccountRepository accountRepository){
         this.userRepository = userRepository;
+        this.transactionRepository = transactionRepository;
+        this.accountRepository = accountRepository;
     }
     @Override
     public List<UserDto> getUserAll() {
         List<User> users = userRepository.findAll();
         List<UserDto> userDtos = new ArrayList<>();
         for (User user: users){
-            userDtos.add(UserMapper.mapToAccountDto(user));
+            userDtos.add(UserMapper.mapToUserDto(user));
         }
         return userDtos;
     }
@@ -29,41 +42,60 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDto getUserById(Long id) {
         User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User does not exists."));
-        return UserMapper.mapToAccountDto(user);
+        return UserMapper.mapToUserDto(user);
     }
 
     @Override
     public UserDto createUser(UserDto userDto) {
-        return null;
+        User user = UserMapper.mapToUser(userDto);
+        User savedUser = userRepository.save(user);
+        return UserMapper.mapToUserDto(savedUser);
     }
 
     @Override
     public void deleteUserById(Long id) {
-
+        userRepository.deleteById(id);
     }
 
     @Override
     public UserDto updateUser(Long id, UserDto userDto) {
-        return null;
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User does not exist."));
+        user.setName(userDto.getName());
+        user.setLastName(userDto.getLastName());
+        user.setUserName(userDto.getUserName());
+        user.setPassword(userDto.getPassword());
+        user.setEmail(userDto.getEmail());
+        user.setPhoneNumber(userDto.getPhoneNumber());
+        user.setPassword(userDto.getPassword());
+        userRepository.save(user);
+        return UserMapper.mapToUserDto(user);
     }
 
     @Override
     public List<AccountDto> getUserAccounts(Long id) {
-        return null;
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User does not exist."));
+        List<Account> accounts = user.getAccounts();
+        List<AccountDto> accountDtos = AccountMapper.mapToAccountDtoList(accounts);
+        return accountDtos;
     }
 
     @Override
     public List<TransactionDto> getUserTransactions(Long id) {
-        return null;
+        // Pull the transactions of the relevant user from the database
+        List<Transaction> transactions = transactionRepository.findByAccountId(id);
+
+        // Convert Transaction objects to TransactionDto and return
+        return transactions.stream()
+                .map(TransactionMapper::mapToTransactionDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public AccountDto addAccountToUser(Long id, AccountDto accountDto) {
-        return null;
-    }
-
-    @Override
-    public TransactionDto addTransactionToUser(Long id, TransactionDto transactionDto) {
-        return null;
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User does not exist."));
+        Account account = AccountMapper.mapToAccountEntity(accountDto);
+        account.setUser(user);
+        accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(account);
     }
 }
