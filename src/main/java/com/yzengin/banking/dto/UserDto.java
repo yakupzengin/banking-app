1d496b291ece4a930683bd7bb46fc929af332eaf
Yakup Zengin <clafedersin@gmail.com>
2024-04-07 03:01:34 +0300

    Added fields for name, lastName, userName, accounts, password, email, phoneNumber, and updatedAt

--- src/main/java/com/yzengin/banking/dto/UserDto.java
+++ src/main/java/com/yzengin/banking/dto/UserDto.java
@@ -1,4 +1,24 @@
 package com.yzengin.banking.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
+    private String name;
+    private String lastName;
+    private String userName;
+    private List<AccountDto> accounts;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime updateAt;
 }
