b378d2e5c3fe8ddea7066fd9f8269aeae8bd53b7
Yakup Zengin <clafedersin@gmail.com>
2024-04-20 13:37:37 +0300

    Added existsByUserNameAndIdNot method

--- src/main/java/com/yzengin/banking/repository/UserRepository.java
+++ src/main/java/com/yzengin/banking/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.yzengin.banking.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    boolean existsByUserNameAndIdNot(String userName, Long id);
 }
