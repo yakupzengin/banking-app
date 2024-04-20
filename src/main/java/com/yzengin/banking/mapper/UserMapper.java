d5a6a54799ebb78dcd510349eae59dafa0bdcb4f
Yakup Zengin <clafedersin@gmail.com>
2024-04-20 13:36:30 +0300

    Update mapToUser and mapToUserDto methods

--- src/main/java/com/yzengin/banking/mapper/UserMapper.java
+++ src/main/java/com/yzengin/banking/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.yzengin.banking.dto.AccountDto;
 import com.yzengin.banking.dto.UserDto;
 import com.yzengin.banking.entity.User;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class UserMapper {
@@ -17,17 +18,20 @@ public class UserMapper {
         user.setEmail(userDto.getEmail());
         user.setPhoneNumber(userDto.getPhoneNumber());
         user.setPassword(userDto.getPassword());
+        user.setCreatedAt(LocalDateTime.now());
+
         return user;
     }
 
     public static UserDto mapToUserDto(User savedUser) {
         UserDto userDto = new UserDto();
+        userDto.setName(savedUser.getName());
         userDto.setUserName(savedUser.getUserName());
         userDto.setLastName(savedUser.getLastName());
         userDto.setPassword(savedUser.getPassword());
         userDto.setEmail(savedUser.getEmail());
         userDto.setPhoneNumber(savedUser.getPhoneNumber());
-        userDto.setUpdateAt(savedUser.getUpdatedAt());
+        userDto.setCreatedAt(savedUser.getCreatedAt());
 
         // Convert accounts field from List<Account> to List<Account Dto> from User class
         List<AccountDto> accountDtos = AccountMapper.mapToAccountDtoList(savedUser.getAccounts());
