de2d1fcdbc5dcd41a6139e6174bf61e48506b94b
Yakup Zengin <clafedersin@gmail.com>
2024-05-17 21:45:31 +0300

    Expanded UserDto for Role Inclusion

--- src/main/java/com/yzengin/banking/dto/UserDto.java
+++ src/main/java/com/yzengin/banking/dto/UserDto.java
@@ -1,11 +1,10 @@
 package com.yzengin.banking.dto;
 
+import com.yzengin.banking.security.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.cglib.core.Local;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -21,4 +20,20 @@ public class UserDto {
     private String email;
     private String phoneNumber;
     private LocalDateTime createdAt;
+    private Role role;
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", accounts=" + accounts +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", createdAt=" + createdAt +
+                ", role=" + role +
+                '}';
+    }
 }
